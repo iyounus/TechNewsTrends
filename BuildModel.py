2d5fdc84ff43cdb015ef30828ce3d8b163510930
iyounus <imranyounus@gmail.com>
2014-11-10 21:21:02 -0800

    reorganizing the code

---
+++ BuildModel.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import pickle as pkl
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import NMF
+
+from nltk import WordNetLemmatizer, word_tokenize
+
+from MongoToDataFrame import MongoToDataFrame
+
+
+
+class Model(object):
+    def __init__(self, df, n_features=5000):
+        self.n_features = n_features
+
+        self.df = df
+
+        self.vectorizer = None
+        self.vector = None
+        self.model = None
+
+        '''
+        Output dir and file names:
+        '''
+        self.outdir = "./" # current dir 
+        self.vect_file1 = self.outdir + "vectorizer.pkl"
+        self.vect_file2 = self.outdir + "vectors.pkl"
+        self.model_file = self.outdir + "model.pkl"
+
+
+    def vectorize(self):
+        vectorizer = TfidfVectorizer(tokenizer=tokenize,
+                                     max_features=self.n_features)
+        vector = vectorizer.fit_transform(self.df.content).toarray()
+        return vectorizer, vector
+
+
+    def build_model(self, n_topics):
+        self.vectorizer, self.vector = self.vectorize()
+        self.model = NMF(n_components=n_topics).fit(self.vector)
+
+
+    def build_models(self, n_topics):
+        '''
+        INPUT list
+        OUTPUT None
+        '''
+        self.model = []
+        for n in n_topics:
+            nmf = NMF(n_components=n).fit(self.vector)
+            self.model.append(nmf)
+
+
+    def pickle_model(self):
+        if self.vector is not None:
+            print "Writing: ", self.vect_file1
+            pkl.dump(self.vectorizer, open(self.vect_file1, "wb"))
+            print "Writing: ", self.vect_file2
+            pkl.dump( self.vector, open(self.vect_file2, "wb"))
+
+        if self.model is not None:
+            print "Writing: ", self.model_file
+            pkl.dump(self.model, open(self.model_file, "wb"))
+
+
+    def pickle_models(self):
+        if self.vector is not None:
+            print "Writing: ", self.vect_file1
+            pkl.dump(self.vectorizer, open(self.vect_file1, "wb"))
+            print "Writing: ", self.vect_file2
+            pkl.dump(self.vector, open(self.vect_file2, "wb"))
+
+        prefix, ext = self.model_file.split('.')
+        if self.model is not None and len(self.model) > 0:
+            for i, model in self.model:
+                outfile = prefix + "_" + str(i) + ".pkl"
+                print "Writing: ", outfile
+                pkl.dump(model, open(outfile, "wb"))
+
+
+
+def stopWords():
+    '''
+    INPUT None
+    OUTPUT list
+    '''
+    with open('StopWords.txt', 'r') as f:
+        stop = [line.strip() for line in f]
+    return stop
+
+
+def tokenize(article):
+    '''
+    INPUT string
+    OUTPUT list
+
+    This is a tokenizer to replace the default tokenizer in TfidfVectorizer
+    '''
+    tokens = [word.lower() for word in word_tokenize(article)]
+
+    tokens = [word for word in tokens if word not in StopW]
+
+    # remove words less than three letters
+    tokens = [word for word in tokens if len(word) >= 3]
+
+    # lemmatize
+    lmtzr = WordNetLemmatizer()
+    tokens = [lmtzr.lemmatize(word) for word in tokens]
+
+    return tokens
+
+
+if __name__ == '__main__':
+    StopW = stopWords()
+
+    df = MongoToDataFrame().GetDataFrame()
+
+    model = Model(df)
+
+    # build only one model
+    model.build_model(10)
+    model.pickle_model()
+
+    # build more that one models with different number of topics
+    #n_topics = range(5,51,5)
+    #model.build_models(n_topics)
+    #model.pickle_models()
---
+++ MongoToDataFrame.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import pickle as pkl
+
+from pymongo import MongoClient
+
+
+class MongoToDataFrame(object):
+    def __init__(self):
+        self.collection = self.init_mongo()
+        self.df = self.create_df()
+
+
+    def init_mongo(self):
+        '''
+        INPUT None
+        OUPUT collection
+
+        This function reads mongodb, nyt_tech, for NYT articles and
+        returns collection articles
+        '''
+        client = MongoClient()
+        db = client.nyt_tech
+        return db.articles
+
+
+    def create_df(self):
+        '''
+        INPUT None
+        OUPUT DataFrame
+
+        Reads mongodb and inserts the required fields in DataFrame
+        '''
+        columns = ['web_url','headline','section_name','pub_date','content']
+        d = {col : [] for col in columns}
+
+        for article in self.collection.find({'content' : {'$exists' : True}}):
+            # Only get articles for 2014, this is temporary
+            if '2014' in article['pub_date']:
+                for col in columns:
+                    if col == 'headline': # headline is a dict
+                        d[col].append(article[col]['main'])
+                    else:
+                        d[col].append(article[col])
+
+        return pd.DataFrame(d)
+
+
+    def save_pickle_df(self, out_file):
+        '''
+        INPUT String
+        OUPUT None
+        '''
+        pkl.dump(self.df, open(out_file, "wb"))
+
+
+    def get_data_frame(self):
+        return self.df
+
+
+if __name__ == '__main__':
+    m2df = MongoToDataFrame()
+    m2df.save_pickle_df("nyt_tech_temp.pkl")
