f765bdba3678009bcb891d95f66a6710f58e6ae1
iyounus <imranyounus@gmail.com>
2014-11-16 16:26:17 -0800

    minor changes

--- word_cloud.py
+++ word_cloud.py
@@ -7,7 +7,7 @@ import sys
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
-from write_web_app_csv import assign_topics
+#from write_web_app_csv import assign_topics
 from utils import stop_words, tokenize
 
 
@@ -31,22 +31,43 @@ def draw_word_cloud(df, topic, n_articles=50, save=True):
         plt.savefig(fileName, bbox_inches='tight')
 
 
+def assign_topics(df, modle, vectors):
+    W = model.components_
+    A = vectors.dot(W.T)
+
+    df['topic'] = list(np.argmax(A, axis=1))
+    df['weight'] = list(np.max(A, axis=1))
+
+    # now sort topics w.r.t number of articles per topic
+    # this is just renaming the topic
+    dg = df[['topic','headline']].groupby('topic')
+    x = sorted(dg.groups.keys())
+    y = [len(dg.groups[i]) for i in x]
+    m = list(np.argsort(y)[::-1])
+    d = {j : x[i] for i, j in enumerate(m)}
+
+    df['topic_sorted'] = df['topic'].map(lambda x : d[x])
+    return df
+
+
 if __name__=="__main__":
-    n_topics = int(sys.argv[1])
+    s_topics = sys.argv[1] # this is a string
+    n_topics = int(s_topics)
 
-    outdir = "topic_browser/static/"
-    modle_file = 'data/model_' + str(n_topics) + '.pkl'
+    outdir = 'topic_browser/static/'
 
-    df = pkl.load(open("data/data_all.pkl"))
-    df = assign_topics(df, modle_file=modle_file,
-                           vector_file='data/vectorizer.pkl')
+    df = pkl.load(open('data/data_all.pkl'))
+    model = pkl.load(open('data/model_' + str(n_topics) + '.pkl'))
+    vectorizer, vectors = pkl.load(open('data/vectorizer.pkl', "rb"))
+
+    df = assign_topics(df, model, vectors)
 
     for i in range(n_topics):
         draw_word_cloud(df, i)
 
     # trim output wordcloud images using imagemagick convert function
     for i in range(n_topics):
-        fileName = outdir + "topic_" + str(i) + "temp.png"
-        os.system("convert " + fileName + " -trim " + \
-                  outdir + "topic_" + str(i) + ".png")
-        os.system("rm " + fileName)
+        fileName = outdir + 'topic_' + str(i) + 'temp.png'
+        os.system('convert ' + fileName + ' -trim ' + \
+                  outdir + 'topic_' + s_topics + '_' + str(i) + '.png')
+        os.system('rm ' + fileName)
